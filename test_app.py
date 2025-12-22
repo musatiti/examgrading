2e5f706ca335926e95443dd242df37a0cf46ca1d
musatiti <mmaltiti22@cit.just.edu.jo>
2025-12-22 16:32:12 +0300

    Update test_app.py

--- test_app.py
+++ test_app.py
@@ -1,8 +1,9 @@
-from app import add_numbers
+#from app import add_numbers
 
-def test_addition_correct():
-    assert add_numbers(2, 3) == 5 # nosec B101
+#def test_addition_correct():
+ #   assert add_numbers(2, 3) == 5 # nosec B101
 
-def test_addition_fail():
+#def test_addition_fail():
     # This test will fail intentionally to demonstrate CI catching it
-    assert add_numbers(2, 3) == 5  # nosec B101
+ #   assert add_numbers(2, 3) == 5  # nosec B101
+return 1
