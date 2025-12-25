0516cefd08695ddafcb8c7126b5c47bc2638b000
musatiti <mmaltiti22@cit.just.edu.jo>
2025-12-25 15:34:42 +0300

    Update app.py

--- app.py
+++ app.py
@@ -71,5 +71,9 @@ def index():
     return render_template_string(HTML, result=result)
 
 if __name__ == "__main__":
-    app.run(debug=False, host="0.0.0.0", port=5000)
+    app.run(
+        host="0.0.0.0",
+        port=5000,
+        debug=False
+    )
 
