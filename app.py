e7105f5120b28502c249a50dba6011015ce517a3
musatiti <mmaltiti22@cit.just.edu.jo>
2026-01-15 02:51:46 +0300

    Update app.py

--- app.py
+++ app.py
@@ -4,10 +4,9 @@ import google.generativeai as genai
 import pytesseract
 from pdf2image import convert_from_bytes
 
-print("GEMINI_API_KEY =", os.getenv("GEMINI_API_KEY"))
 
 genai.configure(api_key=os.getenv("GEMINI_API_KEY"))
-model = genai.GenerativeModel("gemini-1.5-flash")
+model = genai.GenerativeModel("gemini-1.0-pro")
 
 app = Flask(__name__)
 
