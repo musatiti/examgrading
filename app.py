5f44b24cca8bae43cf28bd99c852474bcaf7bfbe
musatiti <mmaltiti22@cit.just.edu.jo>
2026-03-15 12:27:43 +0300

    Update app.py

--- app.py
+++ app.py
@@ -68,4 +68,4 @@ def index():
     return render_template_string(HTML, result=result)
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    app.run(host="0.0.0.0", port=5000)  # nosec B104
